79faf565f4f0784b41b9de69dbcd6dc7b60fd9cf
lsc2966 <73456981+lsc2966@users.noreply.github.com>
2022-06-21 16:36:43 +0900

    Update Swing01.java
    add URL

--- Swing01.java
+++ Swing01.java
@@ -1,7 +1,10 @@
 package swing;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Scanner;
 
 import javax.swing.JButton;
@@ -14,27 +17,36 @@ public class Swing01 extends JFrame {
 	public Swing01(){
 	JFrame f;    
 	//JButton b = new JButton("Click");//create button  
+	
 	initUI();
 	          
 	setSize(400,500);  
 	setLayout(null);  
 	setVisible(true);  
 	}
+	
+	
 	public void initUI(){
 		JButton btn = new JButton("Click");
 		btn.addActionListener(new ActionListener(){ //익명클래스로 리스너 작성
-			public void actionPerformed(ActionEvent e){
-				JButton btn = (JButton) e.getSource();
-				if(btn.getText().equals("Click"))
-					btn.setText("Hello");
-				else 
-					btn.setText("Click");
+			public void actionPerformed(ActionEvent e) {
+				if (Desktop.isDesktopSupported()) {
+					Desktop desktop = Desktop.getDesktop();
+					try {
+						URI uri = new URI("http://www.roseindia.net");
+						desktop.browse(uri);
+					} catch (Exception ex) {
+					}
+				} else {
+				}
 			}
 		});
 		btn.setBounds(130,100,100,40);  
 		add(btn);//adding button on frame  
 	}
 	
+
+
 	public static void main(String[] args) {
 		
 		new Swing01();  
