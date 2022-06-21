ab9298148884a0b1cf6302f64fcdae51212f2b28
lsc2966 <73456981+lsc2966@users.noreply.github.com>
2022-06-21 11:26:47 +0900

    update

--- Swing01.java
+++ Swing01.java
@@ -1,5 +1,7 @@
 package swing;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 import javax.swing.JButton;
@@ -11,14 +13,28 @@ public class Swing01 extends JFrame {
 	
 	public Swing01(){
 	JFrame f;    
-	JButton b = new JButton("click");//create button  
-	b.setBounds(130,100,100,40);  
+	//JButton b = new JButton("Click");//create button  
+	initUI();
 	          
-	add(b);//adding button on frame  
 	setSize(400,500);  
 	setLayout(null);  
 	setVisible(true);  
 	}
+	public void initUI(){
+		JButton btn = new JButton("Click");
+		btn.addActionListener(new ActionListener(){ //익명클래스로 리스너 작성
+			public void actionPerformed(ActionEvent e){
+				JButton btn = (JButton) e.getSource();
+				if(btn.getText().equals("Click"))
+					btn.setText("Hello");
+				else 
+					btn.setText("Click");
+			}
+		});
+		btn.setBounds(130,100,100,40);  
+		add(btn);//adding button on frame  
+	}
+	
 	public static void main(String[] args) {
 		
 		new Swing01();  
